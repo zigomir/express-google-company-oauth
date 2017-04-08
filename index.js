615b10f4a228bd18dc2d754d3e6fb43c5983898d
zigomir <zigomir@gmail.com>
2017-04-07 22:53:24 -0700

    It's a start.

---
+++ index.js
@@ -0,0 +1,48 @@
+var passport = require("passport");
+var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+
+module.exports = function init(app, config) {
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  passport.serializeUser((user, done) => done(null, user));
+  passport.deserializeUser((user, done) => done(null, user));
+
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: config.CLIENT_ID,
+        clientSecret: config.CLIENT_SECRET,
+        callbackURL: config.CALLBACK_URL
+      },
+      function(accessToken, refreshToken, profile, done) {
+        if (
+          (config.CHECK &&
+            config.CHECK instanceof Function &&
+            config.CHECK(profile)) ||
+          profile._json.domain === config.DOMAIN
+        ) {
+          return done(null, profile);
+        } else {
+          return done(null, false);
+        }
+      }
+    )
+  );
+
+  app.get(
+    config.AUTH_PATH,
+    passport.authenticate("google", {
+      scope: config.SCOPE ||
+        "https://www.googleapis.com/auth/plus.profile.emails.read"
+    })
+  );
+
+  app.get(
+    config.CALLBACK_PATH,
+    passport.authenticate("google", {
+      successRedirect: config.SUCCESS_CALLBACK_PATH,
+      failureRedirect: config.FAILURE_CALLBACK_PATH
+    })
+  );
+};
